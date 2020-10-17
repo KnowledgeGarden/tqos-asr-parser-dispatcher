b5bfeb6af964b4b953d640d826fcba7cfc5579ab
Jack Park <jackpark@gmail.com>
2020-10-17 13:56:11 -0700

    adding kafka and more parsers

--- src/main/java/org/topicquests/os/asr/pd/ParserDispatcher.java
+++ src/main/java/org/topicquests/os/asr/pd/ParserDispatcher.java
@@ -7,21 +7,25 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.topicquests.backside.kafka.consumer.api.IMessageConsumerListener;
+import org.topicquests.ks.kafka.KafkaHandler;
 import org.topicquests.os.asr.pd.api.IDispatcher;
 import org.topicquests.os.asr.pd.api.ISentenceParser;
 import org.topicquests.support.ResultPojo;
 import org.topicquests.support.api.IResult;
 
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 
 /**
  * @author jackpark
  *
  */
-public class ParserDispatcher implements IDispatcher {
+public class ParserDispatcher implements IDispatcher, IMessageConsumerListener {
 	private ParserDispatcherEnvironment environment;
 	private List<ISentenceParser> parsers;
-	private final String ParserDispatcherEnvironment = "sentence";
+	private KafkaHandler handler;
 	
 	/**
 	 * @param env
@@ -32,13 +36,19 @@ public class ParserDispatcher implements IDispatcher {
 		
 	}
 
+	protected void setKafkaHandler(KafkaHandler h) {
+		handler = h;
+	}
+
 	@Override
 	public IResult processSentence(JSONObject sentence) {
 		IResult result = new ResultPojo();
 		List<JSONObject> results = new ArrayList<JSONObject>();
-		result.setResultObjectA(sentence);
+		// this carries the original, including docId, paraId, sentId, and text
+		result.setResultObject(sentence);
+		// this carries all the guts of the parses + results here
 		result.setResultObject(results);
-		String text = sentence.getAsString(ParserDispatcherEnvironment);
+		String text = sentence.getAsString("text");
 		IResult r;
 		Iterator<ISentenceParser> itr = parsers.iterator();
 		while (itr.hasNext()) {
@@ -46,18 +56,53 @@ public class ParserDispatcher implements IDispatcher {
 			if (r.getResultObject() != null)
 				results.add((JSONObject)r.getResultObject());
 		}
+		sentence.put("results", results);
 		return result;
 	}
 
 	@Override
 	public void addParser(ISentenceParser parser) {
 		parsers.add(parser);
+		System.out.println("Disp "+parsers);
 	}
 
 	@Override
 	public void shutDown() {
 		// TODO Auto-generated method stub
 
+	}
+
+	boolean processRecord(String jsonRecord) {
+		boolean result = true;
+		JSONObject record = toJSONObject(jsonRecord);
+		// cargo is in the form
+		// { docId, paraId, sentId, text }
+		JSONObject sentence = (JSONObject)record.get("cargo");
+		IResult r = this.processSentence(sentence);
+		if (r.hasError())
+			return false;
+		return result;
+	}
+	
+	@Override
+	public boolean acceptRecord(ConsumerRecord record) {
+		String json = (String)record.value();
+		environment.logDebug("SentenceProcessor.acceptRecord "+json);
+		boolean result = processRecord(json);
+
+		return result;	
+	}
+	
+	JSONObject toJSONObject(String json) {
+		JSONObject result = null;
+		try {
+			JSONParser p = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+			result = (JSONObject)p.parse(json);
+		} catch (Exception e) {
+			e.printStackTrace();
+			environment.logError(e.getMessage(), e);
+		}
+		return result;
 	}
 
 }
--- src/main/java/org/topicquests/os/asr/pd/ParserDispatcherEnvironment.java
+++ src/main/java/org/topicquests/os/asr/pd/ParserDispatcherEnvironment.java
@@ -3,8 +3,16 @@
  */
 package org.topicquests.os.asr.pd;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.topicquests.ks.kafka.KafkaHandler;
 import org.topicquests.os.asr.pd.api.IDispatcher;
+import org.topicquests.os.asr.pd.api.ISentenceParser;
 import org.topicquests.support.RootEnvironment;
+import org.topicquests.support.config.Configurator;
+import org.topicquests.backside.kafka.consumer.api.IMessageConsumerListener;
 
 /**
  * @author jackpark
@@ -12,20 +20,52 @@ import org.topicquests.support.RootEnvironment;
  */
 public class ParserDispatcherEnvironment extends RootEnvironment {
 	private IDispatcher dispatcher;
-	
+	private KafkaHandler consumer;
+	private Map<String,Object>kafkaProps;
+
 	/**
 	 */
 	public ParserDispatcherEnvironment() {
 		super("pd-props.xml", "logger.properties");
 		dispatcher = new ParserDispatcher(this);
+		kafkaProps = Configurator.getProperties("kafka-topics.xml");
+		consumer = new KafkaHandler(this, (IMessageConsumerListener)dispatcher);
+		((ParserDispatcher)dispatcher).setKafkaHandler(consumer);
+		bootDispatcher();
 	}
 
 	/**
 	 * Boot and inject parsers into dispatcher
 	 */
 	private void bootDispatcher() {
-		//TODO
+		List<List<String>> parsers = (List<List<String>>)getProperty("Parsers");
+		//System.out.println("PSRS "+parsers);
+		if (parsers != null && !parsers.isEmpty()) { 
+			Iterator<List<String>> itr = parsers.iterator();
+			List<String> l;
+			ISentenceParser p;
+			try {
+				while (itr.hasNext()) {
+					l = itr.next();
+					Class cls = Class.forName(l.get(1));
+		         
+					// returns the name and package of the class
+					System.out.println("Class found = " + cls.getName());
+					p = (ISentenceParser)cls.newInstance();
+					dispatcher.addParser(p);
+				}
+		      } catch(Exception ex) {
+		    	  ex.printStackTrace();
+		    	  logError(ex.getMessage(), ex);
+		      }
+		}
+	       
 	}
+	
+	public Map<String, Object> getKafkaTopicProperties() {
+		return kafkaProps;
+	}
+
 	
 	public IDispatcher getDispatcher() {
 		return dispatcher;
@@ -34,7 +74,7 @@ public class ParserDispatcherEnvironment extends RootEnvironment {
 	@Override
 	public void shutDown() {
 		System.out.println("ParserDispatcherEnvironment.shutDown");
-		//TODO
+		dispatcher.shutDown();
 
 	}
 
