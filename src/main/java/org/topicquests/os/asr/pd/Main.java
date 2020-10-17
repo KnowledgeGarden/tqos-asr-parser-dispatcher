d9d7adc95fca0bb05802cf393dfde93446c6f069
Jack Park <jackpark@gmail.com>
2020-10-17 14:42:56 -0700

    Added Main and start.sh

---
+++ src/main/java/org/topicquests/os/asr/pd/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.topicquests.os.asr.pd;
+
+/**
+ * @author jackpark
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ParserDispatcherEnvironment();
+	}
+
+}
---
+++ start.sh
@@ -0,0 +1,1 @@
+java -cp ./target/tqos-asr-parser-dispatcher-0.1.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.os.asr.pd.Main
